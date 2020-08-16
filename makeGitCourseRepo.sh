60a7f9073227ef2a588a867c014fc4a23afd8467
Dan Mikhail <danmikhail+github@gmail.com>
2020-08-16 16:09:20 -0500

    Initialize a note repository for TextInformationSystemsCS410

---
+++ makeGitCourseRepo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo
+
+while getopts ":r:" arg; do
+  case $arg in
+    r) repo=$OPTARG;;
+  esac
+done
+
+if [ -z "$repo" ] || [ -d "$repo" ];
+  then
+  echo "\$repo is either unspecified or already exists, use 'bash makeGitCourseRepo.sh -r [REPO_NAME]' to create a new course note Git project";
+  exit 0;
+else
+  mkdir ../$repo;
+  cd ../:$repo;
+  
+  # copy this repo into
+  cp -r ../CourseNoteTemplate/* .;
+  git init;
+  git add .;
+  git commit -m "Initialize a note repository for ${repo}";
+  git remote add origin git@github.com:mikhaidn/${repo}.git;
+  git push -u origin master;
+fi
